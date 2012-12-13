96e8ba535ca22b5175d514fef830d66dde20d3f5
Harriet Lloyd <hattie.lloyd@gmail.com>
2012-12-14 09:08:21 +1030

    Added freezer

--- resources/scr/rooms/freezer.lua
+++ resources/scr/rooms/freezer.lua
@@ -11,13 +11,86 @@ return {
     {
       name = "base",
       animations = {
-        idle = {
+        stocked8 = {
           first = 0,
           last = 0,
           speed = 1,
         },
+         stocked7 = {
+          first = 1,
+          last = 1,
+          speed = 1,
+        },
+        stocked6 = {
+          first = 2,
+          last = 2,
+          speed = 1,
+        },
+        stocked5 = {
+          first = 3,
+          last = 3,
+          speed = 1,
+        },
+        stocked4 = {
+          first = 4,
+          last = 4,
+          speed = 1,
+        },
+         stocked3 = {
+          first = 5,
+          last = 5,
+          speed = 1,
+        },     
+        stocked2 = {
+          first = 6,
+          last = 6,
+          speed = 1,
+        },
+        stocked1 = {
+          first = 7,
+          last = 7,
+          speed = 1,
+        },
+        stocked0 = {
+          first = 8,
+          last = 8,
+          speed = 1,
+        },
+        broken = {
+          first = 9,
+          last = 10,
+          speed = 1,
+        }
       },
-      playing = "idle",
+      playing = "stocked8",
+    },
+   {
+      name = "door",
+      animations = {
+        opened = {
+          first = 0,
+          last = 0,
+          speed = 1,
+        },
+        closed = {
+          first = 3,
+          last = 3,
+          speed = 1,
+        },
+        closing = {
+          first = 0,
+          last = 3,
+          speed = 0.2,
+          goto = "closed",
+        },
+        opening = {
+          first = 3,
+          last = 0,
+          speed = 0.2,
+          goto = "opened",
+        },
+      },
+      playing = "closed",
     },
   },
 }
