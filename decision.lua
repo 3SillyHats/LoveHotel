f2e1957878a912a51807c4bd4e9e59897d690b71
Saxon Douglass <saxon@saxondouglass.com>
2013-01-10 14:43:34 +1030

    134 - made alert popups use START to close instead of A

--- decision.lua
+++ decision.lua
@@ -10,86 +10,46 @@ local decisions = {
   skySpawn = {
     prompt = "Skyfarers are arriving at Love Hotel!" ..
     "\nThey are highly influential so show them a good time.",
-    options = {
-      {
-        text = "Okay",
-        func = nil,
-      },
-    },
+    alert = true,
   },
   groundSpawn = {
     prompt = "Devilspawn are arriving at Love Hotel!" ..
     "\nThey didn't think to bring condoms but they're all horny.",
-    options = {
-      {
-        text = "Okay",
-        func = nil,
-      },
-    },
+    alert = true,
   },
   spaceSpawn = {
     prompt = "Starfarers are arriving at Love Hotel!" ..
     "\nThey have tons of space cash, so lighten their return trip by taking.",
-    options = {
-      {
-        text = "Okay",
-        func = nil,
-      },
-    },
+    alert = true,
   },
   star1 = {
     prompt = "Love Hotel is a luxury hotel for couples. Grow your business " ..
     "by satisfying clients to earn money and reputation. Become the best " ..
     "romantic hotel in the world!",
-    options = {
-      {
-        text = "Okay",
-        func = nil,
-      },
-    },
+    alert = true,
   },
   star2 = {
     prompt = "Good job - 2 stars!\nYou can now build " ..
     "Condom Machines, Spoon suites and Balloon suites.",
-    options = {
-      {
-        text = "Okay",
-        func = nil,
-      },
-    },
+    alert = true,
   },
   star3 = {
     prompt = "Nice work - 3 stars!\nYou can now hire Cooks and build " ..
     "Dining Rooms, Kitchens and Chocolate Moustache suites." ..
     "\nBut now people will be upset if they run out of condoms!",
-    options = {
-      {
-        text = "Okay",
-        func = nil,
-      },
-    },
+    alert = true,
   },
   star4 = {
     prompt = "Excellent - 4 stars!\nYou can now hire Stockers and build " ..
     "Torture suites and Freezers to speed up cooking time." ..
     "\nBut now people will be upset if they get too hungry!",
-    options = {
-      {
-        text = "Okay",
-        func = nil,
-      },
-    },
+    alert = true,
   },
   star5 = {
     prompt = "Amazing - 5 stars!\nYou can now build " ..
     "Nazi Furry suites and Spas so people can have a good time." ..
     "\nGet to 6 stars to become the best love hotel in the world!",
-    options = {
-      {
-        text = "Okay",
-        func = nil,
-      },
-    },
+    alert = true,
   },
 }
 
@@ -118,19 +78,28 @@ local decisionCom = entity.newComponent({
       240,
       "center"
       )
-  
-      for i,option in ipairs(self.decision.options) do
-        if i == self.selected then
-          love.graphics.setColor(255, 255, 255)
-        else
-          love.graphics.setColor(89, 89, 89)
-        end
-        love.graphics.printf(
-          option.text,
-          8, 128 + (16 * i),
+
+      if self.decision.alert then
+      love.graphics.printf(
+          "Press START to continue",
+          8, 128,
           240,
           "center"
         )
+      else
+        for i,option in ipairs(self.decision.options) do
+          if i == self.selected then
+            love.graphics.setColor(255, 255, 255)
+          else
+            love.graphics.setColor(89, 89, 89)
+          end
+          love.graphics.printf(
+            option.text,
+            8, 128 + (16 * i),
+            240,
+            "center"
+          )
+        end
       end
     end
   end,
@@ -170,7 +139,11 @@ end
 -- Handle input on decision screen
 event.subscribe("pressed", 0, function (button)
   if gState == STATE_DECISION then
-    if button == "a" then
+    if decisionCom.decision.alert then
+      if button == "start" then
+        event.notify("state.enter", 0, STATE_PLAY)
+      end
+    elseif button == "a" then
       if decisionCom.result then
         event.notify("state.enter", 0, STATE_PLAY)
       else
