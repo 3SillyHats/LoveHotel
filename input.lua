46bbbf05e51127379e6414212a4fc7d1e8aad083
FinnStokes <finn.stokes@gmail.com>
2015-06-10 13:18:40 +0930

    Fixed input bug preventing joystick buttons from being mapped.

--- input.lua
+++ input.lua
@@ -98,33 +98,35 @@ M.keyReleased = function (key)
 end
 
 M.joystickPressed = function (joystick, button)
+  id = joystick:getID()
   if training then
-    if not map.joysticks[joystick] then
-      map.joysticks[joystick] = {
+    if not map.joysticks[id] then
+      map.joysticks[id] = {
         buttons = {},
         axes = {},
         hats = {},
       }
     end
-    if not map.joysticks[joystick].buttons[button] then
-      map.joysticks[joystick].buttons[button] = inputs[current]
+    if not map.joysticks[id].buttons[button] then
+      map.joysticks[id].buttons[button] = inputs[current]
       trainNext()
     end
     return
   end
   
-  if map.joysticks[joystick] and map.joysticks[joystick].buttons[button] then
-    event.notify("pressed", 0, map.joysticks[joystick].buttons[button])
+  if map.joysticks[id] and map.joysticks[id].buttons[button] then
+    event.notify("pressed", 0, map.joysticks[id].buttons[button])
   end
 end
 
 M.joystickReleased = function (joystick, button)
+  id = joystick:getID()
   if training then
     return
   end
   
-  if map.joysticks[joystick] and map.joysticks[joystick].buttons[button] then
-    event.notify("released", 0, map.joysticks[joystick].buttons[button])
+  if map.joysticks[id] and map.joysticks[id].buttons[button] then
+    event.notify("released", 0, map.joysticks[id].buttons[button])
   end
 end
 
@@ -164,63 +166,64 @@ local oldHat = {}
 
 M.update = function (dt)
   for i, joystick in  ipairs(love.joystick.getJoysticks()) do
-    if not oldAxis[i] then
-      oldAxis[i] = {}
+    id = joystick:getID()
+    if not oldAxis[id] then
+      oldAxis[id] = {}
     end
-    if not oldHat[i] then
-      oldHat[i] = {}
+    if not oldHat[id] then
+      oldHat[id] = {}
     end
     for j=1,joystick:getHatCount() do
-      if not oldHat[i][j] then
-        oldHat[i][j] = 'c'
+      if not oldHat[id][j] then
+        oldHat[id][j] = 'c'
       end
       local hat = joystick:getHat(j)
       if training then
-        if hat ~= oldHat[i][j] and (hat == 'u' or hat == 'd' or hat == 'l' or hat == 'r') then
-          if not map.joysticks[i] then
-            map.joysticks[i] = {
+        if hat ~= oldHat[id][j] and (hat == 'u' or hat == 'd' or hat == 'l' or hat == 'r') then
+          if not map.joysticks[id] then
+            map.joysticks[id] = {
               buttons = {},
               axes = {},
               hats = {},
             }
           end
-          if not map.joysticks[i].hats[hat] then
-            map.joysticks[i].hats[hat] = inputs[current]
+          if not map.joysticks[id].hats[hat] then
+            map.joysticks[id].hats[hat] = inputs[current]
             trainNext()
           end
         end
       else
-        if hat ~= oldHat[i][j] then
-          if map.joysticks[i] then
-            if (hat == 'u' or hat == 'd' or hat == 'l' or hat == 'r') and map.joysticks[i].hats[hat] then
-              event.notify("pressed", 0, map.joysticks[i].hats[hat])
+        if hat ~= oldHat[id][j] then
+          if map.joysticks[id] then
+            if (hat == 'u' or hat == 'd' or hat == 'l' or hat == 'r') and map.joysticks[id].hats[hat] then
+              event.notify("pressed", 0, map.joysticks[id].hats[hat])
             end
-            if (oldHat[i][j] == 'u' or oldHat[i][j] == 'd' or oldHat[i][j] == 'l' or oldHat[i][j] == 'r')
-                and map.joysticks[i].hats[oldHat[i][j]] then
-              event.notify("released", 0, map.joysticks[i].hats[oldHat[i][j]])
+            if (oldHat[id][j] == 'u' or oldHat[id][j] == 'd' or oldHat[id][j] == 'l' or oldHat[id][j] == 'r')
+                and map.joysticks[id].hats[oldHat[id][j]] then
+              event.notify("released", 0, map.joysticks[id].hats[oldHat[id][j]])
             end
           end
         end
       end
       
-      oldHat[i][j] = hat
+      oldHat[id][j] = hat
     end
      for j=1,joystick:getAxisCount() do
-      if not oldAxis[i][j] then
-        oldAxis[i][j] = 0
+      if not oldAxis[id][j] then
+        oldAxis[id][j] = 0
       end
       local axis = joystick:getAxis(j)
-      if axis > 0.2 and oldAxis[i][j] <= 0.2 then
-        joystickPushed(i, j)
-      elseif axis <= 0.2 and oldAxis[i][j] > 0.2 then
-        joystickReleased(i, j)
+      if axis > 0.2 and oldAxis[id][j] <= 0.2 then
+        joystickPushed(id, j)
+      elseif axis <= 0.2 and oldAxis[id][j] > 0.2 then
+        joystickReleased(id, j)
       end
-      if axis < -0.2 and oldAxis[i][j] >= -0.2  then
-        joystickPushed(i, -1-j)
-      elseif axis >= -0.2 and oldAxis[i][j] < -0.2 then
-        joystickReleased(i, -1-j)
+      if axis < -0.2 and oldAxis[id][j] >= -0.2  then
+        joystickPushed(id, -1-j)
+      elseif axis >= -0.2 and oldAxis[id][j] < -0.2 then
+        joystickReleased(id, -1-j)
       end
-      oldAxis[i][j] = axis
+      oldAxis[id][j] = axis
     end
   end
 end
